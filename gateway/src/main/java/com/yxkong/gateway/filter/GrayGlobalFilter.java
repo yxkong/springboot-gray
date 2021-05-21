3229989f85b9584978dcbd7411b56cc9a30129a2
yxkong <5ycode@sina.com>
2021-05-21 22:26:49 +0800

    fixed bug

--- gateway/src/main/java/com/yxkong/gateway/filter/GrayGlobalFilter.java
+++ gateway/src/main/java/com/yxkong/gateway/filter/GrayGlobalFilter.java
@@ -1,13 +1,14 @@
 package com.yxkong.gateway.filter;
 
 import com.alibaba.fastjson.TypeReference;
-import com.google.gson.Gson;
 import com.yxkong.common.constant.HeaderConstant;
 import com.yxkong.common.enums.OrderEnum;
 import com.yxkong.common.utils.FastJsonUtils;
 import com.yxkong.gateway.dto.LoadBalancerRule;
 import com.yxkong.gateway.strategy.RuleStrategy;
 import com.yxkong.lb.holder.GrayHolder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
@@ -19,7 +20,6 @@ import reactor.core.publisher.Mono;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
-import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
@@ -33,6 +33,7 @@ import java.util.Map;
  */
 @Component
 public class GrayGlobalFilter implements GlobalFilter, Ordered {
+    private static Logger log = LoggerFactory.getLogger(GrayGlobalFilter.class);
     @Autowired
     private Map<String, RuleStrategy> ruleStrategyMap;
 
@@ -44,37 +45,42 @@ public class GrayGlobalFilter implements GlobalFilter, Ordered {
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
         }
-        Map<String,String> loginMap = FastJsonUtils.fromJson(info,new TypeReference<Map<String, String>>(){});
-        if(loginMap!= null && !loginMap.isEmpty()){
-            List<LoadBalancerRule> rules = getRules();
-            boolean flag = Boolean.FALSE;
-            /**
-             * 这除规则可以利用表结构来配置化，这个规则，可能是多个字段的组合，也可能只是取模，
-             * 固化一些规则，如：
-             * 注册时间是指定时间的
-             * 登录时间是指定时间的
-             * userId取模,对应几个
-             * 手机号尾号是
-             * 手机号取模
-             */
-            //只实现了并且的关系，只要有一个为false就不走灰度
-            for (LoadBalancerRule r:rules){
-                flag = ruleStrategyMap.get(getRuleName(r.getOperator())).isRoute(r,loginMap);
+        //配置规则与对应的版本
+        String version = "1.0.0";
+        String lable = "normal";
+        try {
+            Map<String,String> loginMap = FastJsonUtils.fromJson(info,new TypeReference<Map<String, String>>(){});
+            if(loginMap!= null && !loginMap.isEmpty()){
+                List<LoadBalancerRule> rules = getRules();
+                boolean flag = Boolean.FALSE;
+                /**
+                 * 这除规则可以利用表结构来配置化，这个规则，可能是多个字段的组合，也可能只是取模，
+                 * 固化一些规则，如：
+                 * 注册时间是指定时间的
+                 * 登录时间是指定时间的
+                 * userId取模,对应几个
+                 * 手机号尾号是
+                 * 手机号取模
+                 */
+                //只实现了并且的关系，只要有一个为false就不走灰度
+                for (LoadBalancerRule r:rules){
+                    flag = ruleStrategyMap.get(getRuleName(r.getOperator())).isRoute(r,loginMap);
+                }
+                if(flag){
+                    //配置规则与对应的版本
+                    version = "2.0";
+                    lable = "gray";
+                    ServerHttpRequest request = exchange.getRequest().mutate()
+                            .header(GrayHolder.VERSION_KEY, version)
+                            .header(GrayHolder.LABEL_KEY,lable)
+                            .build();
+                    return chain.filter(exchange.mutate().request(request).build());
+                }
             }
-            //配置规则与对应的版本
-            String version = "1.0.0";
-            String lable = "normal";
-            if(flag){
-                //配置规则与对应的版本
-                version = "2.0";
-                lable = "gray";
-                ServerHttpRequest request = exchange.getRequest().mutate()
-                        .header(GrayHolder.VERSION_KEY, version)
-                        .header(GrayHolder.LABEL_KEY,lable)
-                        .build();
-                return chain.filter(exchange.mutate().request(request).build());
-            }
-            //不管是灰度还是普通环境都必须初始化标签，要不然会报错
+        }catch (Exception e){
+            log.error("builder gray is error",e);
+        }finally {
+            //不管是灰度还是普通环境都必须初始化标签，要不然在路由时获取标签会报错
             GrayHolder.initHystrixRequestContext(lable,version);
         }
         return chain.filter(exchange.mutate().build());

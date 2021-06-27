8f71d3980fab8918cd4114aafdca1ba2bb56274d
yxkong <5ycode@sina.com>
2021-06-27 10:31:24 +0800

    fixed

--- lb-common/src/main/java/com/yxkong/common/configuration/FeignConfiguration.java
+++ lb-common/src/main/java/com/yxkong/common/configuration/FeignConfiguration.java
@@ -1,5 +1,7 @@
 package com.yxkong.common.configuration;
 
+import com.yxkong.common.plugin.context.SecurityContextHolder;
+import com.yxkong.common.plugin.context.SecurityDTO;
 import com.yxkong.lb.holder.GrayHolder;
 import feign.Logger;
 import feign.RequestInterceptor;
@@ -27,8 +29,12 @@ public class FeignConfiguration {
         return new RequestInterceptor(){
             @Override
             public void apply(RequestTemplate template) {
-                template.header(GrayHolder.LABEL_KEY, GrayHolder.getLable());
-                template.header(GrayHolder.VERSION_KEY, GrayHolder.getVersion());
+                /**
+                 * 使用SecurityContextHolder，确保在启用线程池时，子线程feign也能获取到lable和version
+                 */
+                final SecurityDTO dto = SecurityContextHolder.getSecurityDTO();
+                template.header(GrayHolder.LABEL_KEY, dto.getLabel());
+                template.header(GrayHolder.VERSION_KEY, dto.getVersion());
             }
         };
     }
--- lb-common/src/main/java/com/yxkong/lb/configuration/ServletAutoConfiguration.java
+++ lb-common/src/main/java/com/yxkong/lb/configuration/ServletAutoConfiguration.java
@@ -1,5 +1,6 @@
 package com.yxkong.lb.configuration;
 
+import com.yxkong.common.plugin.context.SecurityContextHolder;
 import com.yxkong.lb.holder.GrayHolder;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Bean;
@@ -37,15 +38,23 @@ public class ServletAutoConfiguration {
             return new HandlerInterceptor() {
                 @Override
                 public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-                    //SecurityContextHolder.clearContext();
-                    //SecurityContextHolder.setRequest(request);
+                    /**
+                     * 使用InheritableThreadLocal 保证在开启多线程时，子线程的feign调用能获取到缓存的header值
+                     */
+                    SecurityContextHolder.clearContext();
+                    SecurityContextHolder.setRequest(request);
+                    /**
+                     * initHystrixRequestContext 只能保证Hystrix内部，如果异步或者开启多线程
+                     * 子线程是拿不到结果的，如果是线程池调用，在任务里重新进行initHystrixRequestContext
+                     */
+
                     GrayHolder.initHystrixRequestContext(request.getHeader(GrayHolder.LABEL_KEY),request.getHeader(GrayHolder.VERSION_KEY));
                     return true;
                 }
 
                 @Override
                 public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-                    //SecurityContextHolder.clearContext();
+                    SecurityContextHolder.clearContext();
                     GrayHolder.shutdownHystrixRequestContext();
                 }
             };
